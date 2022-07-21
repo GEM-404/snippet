3ba0a26a273a7eb331700bc2919c9ce552e0f97d
GEM-404 <mashaephantus2000@gmail.com>
2022-07-21 03:26:05 +0300

    Added User authentication to the web app and user access restriction rules

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -11,6 +11,10 @@ import (
 	"github.com/go-playground/form/v4"
 )
 
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+}
+
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 
@@ -56,8 +60,9 @@ func (app *application) render(w http.ResponseWriter, status int, page string, d
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
